022056619041c5338f96e00c735d67f58d425664
Amit Batra <amit.batra@gmail.com>
2026-06-10 16:44:16 +0530

    Day 1 progress

---
+++ day_01/02_graph_with_two_nodes.py
@@ -0,0 +1,37 @@
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+
+class State(TypedDict):
+    message: str
+
+
+def hello_node(state: State):
+    return {
+        "message": "Hello"
+    }
+
+
+def world_node(state: State):
+    return {
+        "message": state["message"] + ", World!"
+    }
+
+
+builder:StateGraph = StateGraph(State)
+
+builder.add_node("hello", hello_node)
+builder.add_node("world", world_node)
+
+builder.add_edge(START, "hello")
+builder.add_edge("hello", "world")
+builder.add_edge("world", END)
+
+graph = builder.compile()
+
+initial_state: State = {}
+final_state: State = graph.invoke(initial_state)
+
+print(final_state)
+
+print(graph.get_graph().draw_ascii())
---
+++ day_01/03_homework.py
@@ -0,0 +1,39 @@
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+
+class State(TypedDict):
+    name: str
+    age: int
+
+
+def name_node(state: State):
+    return {
+        "name": "Amit Batra"
+    }
+
+
+def age_node(state: State):
+    return {
+        "name": state["name"],
+        "age": 48
+    }
+
+
+builder:StateGraph = StateGraph(State)
+
+builder.add_node("name_node", name_node)
+builder.add_node("age_node", age_node)
+
+builder.add_edge(START, "name_node")
+builder.add_edge("name_node", "age_node")
+builder.add_edge("age_node", END)
+
+graph = builder.compile()
+
+initial_state: State = {}
+final_state: State = graph.invoke(initial_state)
+
+print(final_state)
+
+print(graph.get_graph().draw_ascii())
