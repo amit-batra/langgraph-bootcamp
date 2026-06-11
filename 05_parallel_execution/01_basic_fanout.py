ab9b2a9e7282af494fecb4aeb45d15eac6b676c4
Amit Batra <amit.batra@gmail.com>
2026-06-11 22:26:13 +0530

    Implemented basic fan-out pattern in LangGraph

---
+++ 05_parallel_execution/01_basic_fanout.py
@@ -0,0 +1,89 @@
+# This script illustrates the basic fan-out pattern in which multiple nodes
+# execute in parallel and then combine in the next step.
+#                   +-----------+                    
+#                   | __start__ |                    
+#                  *+-----------+***                 
+#               ***        *        ***              
+#           ****           *           ****          
+#         **               *               **        
+# +--------+          +--------+          +--------+ 
+# | node_a |          | node_b |          | node_c | 
+# +--------+****      +--------+       ***+--------+ 
+#               ***        *        ***              
+#                  ****    *    ****                 
+#                      **  *  **                     
+#                 +---------------+                  
+#                 | combine_nodes |                  
+#                 +---------------+                  
+#                          *                         
+#                          *                         
+#                          *                         
+#                     +---------+                    
+#                     | __end__ |                    
+#                     +---------+                    
+
+from json import dumps
+from time import time
+from typing import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.state import CompiledStateGraph
+
+class State(TypedDict):
+    step_a_result: str
+    step_b_result: str
+    step_c_result: str
+
+def step_a(state: State) -> State:
+    # Fetch the current timestamp
+    timestamp: float = time()
+    return {
+        "step_a_result": f"Step A at {timestamp}"
+    }
+
+def step_b(state: State) -> State:
+    # Fetch the current timestamp
+    timestamp: float = time()
+    return {
+        "step_b_result": f"Step B at {timestamp}"
+    }
+
+def step_c(state: State) -> State:
+    # Fetch the current timestamp
+    timestamp: float = time()
+    return {
+        "step_c_result": f"Step C at {timestamp}"
+    }
+
+def combine(state: State) -> State:
+    formatted_output: str = dumps(state, indent=2, sort_keys=True)
+    print(f"Final state: {formatted_output}")
+
+def create_compiled_graph() -> CompiledStateGraph:
+    builder: StateGraph = StateGraph(State)
+
+    builder.add_node("node_a", step_a)
+    builder.add_node("node_b", step_b)
+    builder.add_node("node_c", step_c)
+    builder.add_node("combine_nodes", combine)
+
+    builder.add_edge(START, "node_a")
+    builder.add_edge(START, "node_b")
+    builder.add_edge(START, "node_c")
+
+    builder.add_edge("node_a", "combine_nodes")
+    builder.add_edge("node_b", "combine_nodes")
+    builder.add_edge("node_c", "combine_nodes")
+
+    builder.add_edge("combine_nodes", END)
+
+    return builder.compile()
+
+def main() -> None:
+    graph: CompiledStateGraph = create_compiled_graph()
+    print(graph.get_graph().draw_ascii())
+
+    graph.invoke({})
+
+if __name__ == "__main__":
+    main()
